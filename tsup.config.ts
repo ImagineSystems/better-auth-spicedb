51e89a481e302767cf6267355b70270803a429f7
Ubayd Khan <ubaydkh@gmail.com>
2025-11-28 21:27:29 +0000

    fix: exclude native modules & peer deps from bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/client.ts'], 
+  entry: {
+    index: 'src/index.ts',
+    client: 'src/client.ts',
+  },
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  external: [
+      'better-auth', 
+      'better-auth/svelte', 
+      'better-auth/client',
+      '@authzed/authzed-node', 
+      'google-protobuf'       
+  ],
 });
